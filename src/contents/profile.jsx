026e8533fdc81dc3fe7289722d8856ad76477426
pvekxx <jonghyuk0186@gmail.com>
2024-11-27 16:15:40 +0900

    commit

--- src/contents/profile.jsx
+++ src/contents/profile.jsx
@@ -35,9 +35,8 @@ const Profile = () => {
     return (
         <div
             ref={animatedElementRef}
-            className={`flex flex-row items-center justify-between gap-[38px] transition-opacity duration-1000 ease-in-out ${
-                isVisible ? 'opacity-100' : 'opacity-0'
-            }`}
+            className={`flex flex-row items-center justify-between gap-[38px] transition-opacity duration-1000 ease-in-out ${isVisible ? 'opacity-100' : 'opacity-0'
+                }`}
         >
             <div className="w-[280px] md:w-[28%] ">
                 <img
@@ -49,7 +48,7 @@ const Profile = () => {
             </div>
             <div className="w-[100%] box-border md:w-[70%] md:p-[10px] ">
                 <Hero>
-                    곽인구 <span className="text-[16px] text-slate-400 font-normal">web7722</span>
+                    안녕하세요 <span className="text-[16px] text-slate-400 font-normal">web7722</span>
                 </Hero>
                 <p className="text-slate-700 pt-[5px] md:text-[24px] md:pt-[10px] text-[16px]">
                     아주 작은 개발자가 될거야
